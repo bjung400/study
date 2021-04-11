bf808d92d9bbbcfc682b75a6610716b3a3edea4c
bjung <jongbo400@gmail.com>
2021-04-12 01:42:03 +0900

    Card Implement

--- bjung_file/Rummikub/Rummikub.c
+++ bjung_file/Rummikub/Rummikub.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct card
+{
+    int number[104];
+    int color[4]; //blue, red, orange, black
+};
 
 int group(int card1, int card2, int card3, int card4);
 int run(int card1, int card2, int card3, int card4);
 
 int main(void)
 {
-    int blue_card[14] = {1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10,
-    11, 12, 13};
+    struct card c;
+
+    int n = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        c.color[i] = i;
+        for (int j = 0; c.color[i] = i; j++)
+        {
+            for (int k = 1; k <= 13; k++) 
+            {
+                c.number[n] = k;
+                c.color[n] = i;
+                n++;
+            }
+        }    
+    }
+
     int red_card[14] = {1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10,
     11, 12, 13};
     int orange_card[14] = {1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10,
@@ -29,6 +49,10 @@ int main(void)
     run(11, 12, 13, 15);
        
 }
+    for(int i = 0; i < 106; i++)
+    {
+        for(int j = 0; )
+    }
 int group(int card1, int card2, int card3, int card4)
 {
     int count = -7;
