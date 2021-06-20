c2916f32ee579973effbe5de0397c1bf4f2d80f8
bjung <bjung@bjungui-MacBookPro.local>
2021-06-21 00:18:39 +0900

    정리

--- bjung_file/baekjoon/11654.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    char c;
-    scanf("%s", &c);
-    printf("%d", c);
-}
--- jong_file/extern/test.c
+++
@@ -1,8 +0,0 @@
-int num1=5;
-int num2=10;
-int num3=20;
-
-void add(void)
-{
-	num3=num1+num2;
-}
