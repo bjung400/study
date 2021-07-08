d14b1f12ce1478239d4906a153ed8320a5d7403d
kk <kkkk@kkui-MacBookAir.local>
2021-07-08 20:39:56 +0900

    jump to python study 21.07.08

--- Python/jump_to_python/test.py
+++ Python/jump_to_python/test.py
@@ -1,13 +1,1 @@
-'''
-def positive(l):
-    result = []
-    for i in l:
-        if i > 0:
-            result.append(i)
-    return result
-'''
-def positive(x):
-    return x > 0
-    
-print(list(filter(positive, [1, -0, 2, -5, 3])))
-print(list(filter(lambda x: x > 0, [1, -0, 2, -5, 3])))
+print(list((range(0, -10, -3))))
