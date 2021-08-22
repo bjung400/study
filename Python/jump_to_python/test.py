feb9bfb402a86ce2c2f239798d8eedb26429feb6
bjung <bjung@bjungui-MacBookPro.local>
2021-08-22 22:02:35 +0900

    jump to python ps

--- Python/jump_to_python/test.py
+++ Python/jump_to_python/test.py
@@ -1,27 +1,19 @@
-# data = '''
-# park 800905-1049118
-# kim  700905-1059119
-# '''
+# q1 문자열 바꾸기
+# str = "a:b:c:d"
+# print('#'.join(str.split(':')))
 
-# result = []
-# for line in data.split('\n'):
-#     word_result = []
-#     for word in line.split(" "):
-#         if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit():
-#             word = word[:6] + "-" + "*******"
-#         word_result.append(" ".join(word_result))
-# print("\n".join(result))
-data = """
-park 800905-1049118
-kim  700905-1059119
-"""
+# q2 딕셔너리 값 추출하기
+# get 함수를 사용하면 해당 key가 없을 경우에는 두번째 매개변수로 전달된 default 값을 대신 돌려준다.
+# a = {'A':90, 'B':80}
+# print(a.get('C', 70))
 
-result = []
-for line in data.split("\n"):
-    word_result = []
-    for word in line.split(" "):
-        if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit():
-            word = word[:6] + "-" + "*******"
-        word_result.append(word)
-    result.append(" ".join(word_result))
-print("\n".join(result))
+# q3 리스트의 더하기와 extend 함수
+# a = [1, 2, 3]
+# a = a + [4, 5]
+# print(a)
+
+a = [1, 2, 3]
+a = a + [4, 5]
+# a.extend([4, 5]) # 리스트 끝에 iterable의 모든 항목을 넣습니다.
+print(id(a))
+
