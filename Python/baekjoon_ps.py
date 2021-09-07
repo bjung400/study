04e23837eb4b3ccc43db16403fde5c82cda3b9d5
bjung <bjung@bjungui-MacBookPro.local>
2021-09-08 03:05:51 +0900

    python ps

--- Python/baekjoon_ps.py
+++ Python/baekjoon_ps.py
@@ -1,15 +1,28 @@
 # 2920
-music = input().split()
+# music = input().split()
     
-ascending = [i for i in range(1, 9)]
+# ascending = [i for i in range(1, 9)]
 
-music = list(map(int, music))
+# music = list(map(int, music))
 
-if music == ascending: print("ascending")
-elif music == ascending[::-1]: print("descending")
-else: print("mixed")
+# if music == ascending: print("ascending")
+# elif music == ascending[::-1]: print("descending")
+# else: print("mixed")
 
+# 2475
+# # n = list(map(int, input().split()))
 
-    
-        
-    
+# print(sum(int(n)*int(n) for n in input()[::2]) % 10)
+
+# 2908
+# # print(sorted(input()[::-1].split(), reverse=True)[0])
+# print(max(input()[::-1].split()))
+
+# 1018
+n, m = map(int, input().split())
+list = []
+mini = []
+
+for i in range(n): list.append(input())
+
+print(list)
