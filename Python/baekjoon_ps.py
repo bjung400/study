01bd96810adf537bab16fc48286702474e3df844
kk <kkkk@kkui-MacBookAir.local>
2021-09-15 21:35:37 +0900

    python ps

--- Python/baekjoon_ps.py
+++ Python/baekjoon_ps.py
@@ -19,37 +19,63 @@
 # print(max(input()[::-1].split()))
 
 # 1018
-N, M = map(int, input().split())
-original = []
-count = []
+# N, M = map(int, input().split())
+# original = []
+# count = []
 
-[original.append(input()) for i in range(N)]
+# [original.append(input()) for i in range(N)]
 
-for a in range(N-7):
-    for b in range(M-7):
-        index1 = 0
-        index2 = 0
+# for a in range(N-7):
+#     for b in range(M-7):
+#         index1 = 0
+#         index2 = 0
 
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
-                        index2 += 1
-                else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
-                        index2 += 1
-        count.append(min(index1, index2))
+#         for i in range(a, a+8):
+#             for j in range(b, b+8):
+#                 if (i+j) % 2 == 0:
+#                     if original[i][j] != 'W':
+#                         index1 += 1
+#                     if original[i][j] != 'B':
+#                         index2 += 1
+#                 else:
+#                     if original[i][j] != 'B':
+#                         index1 += 1
+#                     if original[i][j] != 'W':
+#                         index2 += 1
+#         count.append(min(index1, index2))
 
-count.append(min(index1, index2))
+# count.append(min(index1, index2))
 
-print(min(count))
-# https://bambbang00.tistory.com/43
+# print(min(count))
         
 
 # example = ["WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW"]
 # example_2 = ["BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB"]
+
+N, M = map(int, input().split())
+
+original = []
+count = []
+
+[original.append(input()) for _ in range(N)]
+
+for a in range(N - 7):
+    for b in range(M - 7):
+        check1 = 0
+        check2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if original[i][j] == 'W':
+                        check1 += 1
+                    if original[i][j] == 'B':
+                        check2 += 1
+                else:
+                    if original[i][j] == 'B':
+                        check1 += 1
+                    if original[i][j] == 'W':
+                        check2 += 1
+        
+            
+                    
 
--- Python/test.py
+++ Python/test.py
@@ -1,23 +1,3 @@
-iterable, visited = [1, 2, 3, 4, 5], [False] * 5
-
-def get_permutation( _currentindex, _permutation):
-    if _currentindex == 5:
-        print(_permutation)
-        return
-
-    for index, value in enumerate(iterable):
-        if not visited[index]:
-            visited[index], _permutation[_currentindex] = True, value
-            get_permutation(_currentindex + 1, _permutation)
-            visited[index] = False
-
-if __name__ == '__main__':
-    get_permutation(0, [None] * 5)
-
-import itertools
-
-iterable = [1, 2, 3, 4, 5]
-result = itertools.permutations(iterable)
-for permutation in result:
-    print(list(permutation))
-    
+a = 0
+for i in range(a, a+8):
+    print(i)
